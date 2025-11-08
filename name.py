8e309d940a9bc0bea277fb05718e662f79013c4d
Jasmine09051002 <3768712923@qq.com>
2025-11-08 14:52:43 +0800

    提交培训1

---
+++ name.py
@@ -0,0 +1,49 @@
+print("My English name is Jasmine")
+x=905
+y=1002
+print(x,"+",y,"=",x+y)
+
+print("Zayne        Jasmine\n","",sep="     --- ---\n"
+                         "   ----- -----\n"
+                          "      ------\n "
+                            "       -- \n     ")
+
+print("Jasmine")#Task 1
+
+print("all delicious foods","green or blue")#Task 2
+
+print("age 18\n"
+      "I don't want to reveal my height\n "
+      "also weight\n")#Task 3
+
+
+
+school_name="HNU"
+print("我在"+school_name+"学习")#Task 4
+
+print(
+"*****\n"
+"****\n"
+"***\n"
+"**\n"
+"*")#Task 5
+
+
+
+name="Jasmine"
+hobby="looking book"
+print(name+" enjoys "+hobby)#Task 6
+
+print ("I am learning\n"
+       "Python\n"
+       "programming\n")#Task 7
+
+print("apple","banana","cherry",sep="-")#Task 8
+
+print ('"My favorite movie is LaLa Land')#Task 9
+
+print("  *")
+print(" **")
+
+
+
